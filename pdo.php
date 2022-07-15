d33b0322d4cebc391ce3cf2fd1cc74972db1259c
SorfinaNorly <73321037+SorfinaNorly@users.noreply.github.com>
2022-07-15 21:35:20 +0800

    Add files via upload

---
+++ pdo.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang = "en">
+
+<head>  
+</head>
+
+<body>
+<?php
+
+//Connect PHP with MYSQL
+$pdo = new PDO('mysql:host=localhost;dbname=adminbackend','root','');
+//MYSQL statement or command
+$stmt = $pdo->query("SELECT * FROM registerseminar");
+//Display the output
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<table border>'."\n";
+foreach ( $rows as $row ) {
+    echo "<tr><td>";
+    echo($row['topic']);
+    echo("</td><td>");
+    echo($row['date']);
+    echo("</td><td>");
+    echo($row['time']);
+    echo("</td><td>");
+    echo($row['venue']);
+    echo("</td></tr>\n");
+}
+echo "</table>\n";
+?>
+</body>
+</html>
