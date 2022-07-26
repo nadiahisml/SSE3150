e7fac3448d81e8f08b1472b3c1c0292fafd6fc29
nadiahisml <73320972+nadiahisml@users.noreply.github.com>
2022-07-27 02:21:10 +0800

    Admin Login

--- interval.php
+++ interval.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-	<meta charset="UTF-8">
+	
+    <meta charset="UTF-8">
 	<link rel="stylesheet" href=
 "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,31 +10,29 @@
 	<link rel="stylesheet" href="login.css">
 	<title>Login Page</title>
 </head>
-
 <body>
-	<form action="validate.php" method="post">
+
+    <form action="validate.php" method="post">
 		<div class="login-box">
 			<h1>Login</h1>
-
 			<div class="textbox">
 				<i class="fa fa-user" aria-hidden="true"></i>
 				<input type="text" placeholder="Username"
 						name="username" value="">
 			</div>
-
 			<div class="textbox">
 				<i class="fa fa-lock" aria-hidden="true"></i>
 				<input type="password" placeholder="Password"
 						name="password" value="">
 			</div>
-
 			<input class="button" type="submit"
 					name="login" value="Sign In">
 		</div>
 	</form>
-
 	<p>Username: admin 
 		Password: admin</p>
-</body>
 
+</body>
+</html>
+</body>
 </html>
