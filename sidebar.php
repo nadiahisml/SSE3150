4459fee705d4decf17f4bcdbb1a6729390c58f07
nadiahisml <73320972+nadiahisml@users.noreply.github.com>
2022-07-28 13:36:09 +0800

    Admin Dashboard

--- sidebar.php
+++ sidebar.php
@@ -1,13 +1,13 @@
 <!-- Sidebar -->
 <div class="sidebar" id="mySidebar">
 <div class="side-header">
-    <img src="./assets/images/logo.png" width="120" height="120" alt="Swiss Collection"> 
+    <img src="logo.png" width="120" height="120" alt="Swiss Collection"> 
     <h5 style="margin-top:10px;">Hello, Admin</h5>
 </div>
 
 <hr style="border:1px solid; background-color:#8a7b6d; border-color:#3B3131;">
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
-    <a href="./adminpage.php" ><i class="fa fa-home"></i> Dashboard</a>
+    <a href="./adminPage.php" ><i class="fa fa-home"></i> Dashboard</a>
     <a href="#changePassword"  onclick="showChangePassword()" ><i class="fa fa-users"></i> Change password</a>
     <a href="#suspendUsers"   onclick="showSuspendUsers()" ><i class="fa fa-th-large"></i> Suspend users</a>
     <a href="#addAdmin"   onclick="showAddAdmin()" ><i class="fa fa-th"></i> Add admin</a>
