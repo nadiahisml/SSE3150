bf32d3e7b4bac08cd4b43ae76bc6e4b25480bdf7
nadiahisml <73320972+nadiahisml@users.noreply.github.com>
2022-07-28 13:47:25 +0800

    Activity Logs

---
+++ activityLogs.php
@@ -0,0 +1,28 @@
+
+<div >
+  <h2>Activity Logs</h2>
+  <table class="table ">
+    <thead>
+      <tr>
+        <th class="text-center">No</th>
+        <th class="text-center">Date</th>
+        <th class="text-center">Activity</th>
+      </tr>
+    </thead>
+      <tr>
+        <td class="text-center">1</td>
+        <td class="text-center">13/07/2022</td>
+        <td class="text-center">Change password</td>
+      </tr>
+      <tr>
+        <td class="text-center">2</td>
+        <td class="text-center">14/07/2022</td>
+        <td class="text-center">Suspend user</td>
+      </tr>
+      <tr>
+        <td class="text-center">3</td>
+        <td class="text-center">15/07/2022</td>
+        <td class="text-center">Register seminar</td>
+      </tr>
+  
+  </table>
